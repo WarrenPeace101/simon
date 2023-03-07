e9e2139eee3afd825f797e36e9b8da4c7843e0ca
WarrenPeace101 <mangum.warren99@gmail.com>
2023-03-06 17:53:46 -0700

    changed a few more settings to match the HTML

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() {
     const nameEl = document.querySelector("#name");
-    localStorage.setItem("playerUsername", nameEl.value);
+    localStorage.setItem("username", nameEl.value);
     window.location.href = "play.html";
 }
